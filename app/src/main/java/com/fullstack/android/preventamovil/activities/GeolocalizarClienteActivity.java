0b005e3221825de6a4911df929e37bbfcad2b876
carloscabrerax <cadacaes@hotmail.com>
2016-11-26 13:07:18 -0300

    geolocalizar cliente

--- app/src/main/java/com/fullstack/android/preventamovil/activities/GeolocalizarClienteActivity.java
+++ app/src/main/java/com/fullstack/android/preventamovil/activities/GeolocalizarClienteActivity.java
@@ -3,26 +3,17 @@ package com.fullstack.android.preventamovil.activities;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
-
-//Hola
 public class GeolocalizarClienteActivity implements LocationListener {
     @Override
     public void onLocationChanged(Location location) {
-
     }
-
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
     }
-
     @Override
     public void onProviderEnabled(String provider) {
-
     }
-
     @Override
     public void onProviderDisabled(String provider) {
-
     }
 }
